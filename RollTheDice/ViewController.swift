eba78a6aad0a310f711da03ef8f61d40b7434243
Bold Lion <d@ds-mac-mini.local>
2018-11-08 21:47:01 +0200

    Initial Commit

---
+++ RollTheDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RollTheDice
+//
+//  Created by Bold Lion on 8.11.18.
+//  Copyright © 2018 Bold Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

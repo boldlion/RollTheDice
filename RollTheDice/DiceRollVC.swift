54d6772b40bca39ec20944c07f0f06c15ff7faf0
boldlion <boldlion1988@gmail.com>
2019-07-16 11:31:43 +0300

    Finished project

--- RollTheDice/DiceRollVC.swift
+++ RollTheDice/DiceRollVC.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DiceRollVC: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let diceImages = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-
-    @IBAction func rollTapped(_ sender: UIButton) {
+        changeImages()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeImages()
+    }
+    
+    @IBAction func rollTapped(_ sender: UIButton) {
+        changeImages()
+    }
+    
+    func changeImages() {
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        diceImageView1.image = UIImage(named: diceImages[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceImages[randomDiceIndex2])
+    }
 }
 
